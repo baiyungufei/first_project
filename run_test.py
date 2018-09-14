8181d534d1506a0510a92f416726eb341b026a07
baiyungufei <chang.cai@ucsf.edu>
2018-09-14 16:38:13 -0700

    second changes to a file

--- run_test.py
+++ run_test.py
@@ -2,5 +2,5 @@ import numpy as np
 
 age = np.loadtxt('participants.tsv', skiprows=1, usecols=1)
 mean_age = sum(age)/len(age)
-np.savetext('demeandd_age.txt', age-mean_age)
+np.savetxt('demeandd_age.txt', age-mean_age)
 print('done')
